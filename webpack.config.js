82d5636ec2dfeb3bcccc6681a8126e123eccf638
Naooki <sheynikovea@gmail.com>
2018-01-25 00:23:04 +0300

    initial angular + webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: {
+    polyfills: './src/polyfills.ts',
+    vendor: './src/vendor.ts',
+    app: './src/main.ts'
+  },
+
+  output: {
+    filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist')
+  },
+
+  resolve: {
+    extensions: ['.ts', '.js', 'html', 'css']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loaders: [ 'ts-loader', 'angular2-template-loader' ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+        loader: 'file-loader?name=assets/[name].[hash].[ext]'
+      },
+      /* Embed files. */
+      { 
+        test: /\.(html|css)$/, 
+        loader: 'raw-loader',
+        exclude: /\.async\.(html|css)$/
+      },
+      /* Async loading. */
+      {
+        test: /\.async\.(html|css)$/, 
+        loaders: ['file?name=[name].[hash].[ext]', 'extract']
+      }
+    ]
+  },
+
+  plugins: [
+    // Workaround for angular/angular#11580
+    new webpack.ContextReplacementPlugin(
+      /angular(\\|\/)core(\\|\/)@angular/,
+      path.resolve(__dirname, '../src')
+    ),
+
+    new webpack.optimize.CommonsChunkPlugin({
+      name: ['app', 'vendor', 'polyfills']
+    }),
+
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
+};
