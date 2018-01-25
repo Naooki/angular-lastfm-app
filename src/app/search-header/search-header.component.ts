ff52c9e5e95b45f69b2d492b432cc6078728b4b3
Yahor Sheinikau <Yahor_Sheinikau@epam.com>
2018-01-25 19:12:36 +0300

    adds precompiled angular material theme

--- src/app/search-header/search-header.component.ts
+++ src/app/search-header/search-header.component.ts
@@ -3,5 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'lastfm-header',
   templateUrl: './search-header.component.html',
+  styleUrls: ['./search-header.scss']
 })
-export class SearchHeaderComponent { }
+export class SearchHeaderComponent { 
+    isSearching = false;
+
+    constructor() {}
+
+    triggerSearching() {
+        this.isSearching = !this.isSearching;
+    }
+}
